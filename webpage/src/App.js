357d06a1eebccfe365efe3134f7804b22fde577f
rguarnizo <rguarnizo@unal.edu.co>
2021-03-12 17:24:05 -0500

    Tree.js Add camera, render and scene

--- webpage/src/App.js
+++ webpage/src/App.js
@@ -1,15 +1,32 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Styles/app.scss";
-import {FirstPage} from "./Pages/FirstPage";
+
+import * as THREE from "three";
 
 
 function App() {
-  return (
-    <div className="row fpage-container d-flex align-items-center">
-      <FirstPage>
 
-      </FirstPage>
-    </div>
+  var camera,scene,renderer;
+
+  scene = new THREE.Scene();
+
+  camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth/window.innerHeight
+  );
+  
+  renderer = new THREE.WebGL1Renderer();
+  renderer.setSize(window.innerWidth,window.innerHeight);
+
+  renderer.render(scene,camera);
+
+    document.body.appendChild(renderer.domElement);
+
+
+  return (
+<>
+  
+</>
   );
 }
 
