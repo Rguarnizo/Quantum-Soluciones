6c21c3dae3a970119604232c5d545ce3738c8f1e
rguarnizo <rguarnizo@unal.edu.co>
2021-03-12 17:07:23 -0500

    Instalando libreria Tree-js

--- webpage/src/App.js
+++ webpage/src/App.js
@@ -1,13 +1,22 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import FirstPage from "./Pages/FirstPage";
 import "./Styles/app.scss";
+import {FirstPage} from "./Pages/FirstPage";
+
 
 function App() {
   return (
     <div className="row fpage-container d-flex align-items-center">
-      <FirstPage />
+      <FirstPage>
+
+      </FirstPage>
     </div>
   );
 }
+
+
+
+
+
+
 
 export default App;
