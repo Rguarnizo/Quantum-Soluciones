60259c376a3a863b61745c788b43b0d444c48eee
rguarnizo <rguarnizo@unal.edu.co>
2021-03-12 16:42:11 -0500

    Probando tree.js

--- webpage/src/Pages/FirstPage.jsx
+++ webpage/src/Pages/FirstPage.jsx
@@ -4,16 +4,16 @@ import "../Styles/firstPage.scss";
 /*eslint no-unused-vars: "off"*/
 
 const FirstPage = () => {
-  // const videosSource = [
-  //   "https://firebasestorage.googleapis.com/v0/b/quantum-68439.appspot.com/o/videos%2F200415_01_Camera_Equipment_009.mp4?alt=media&token=3320b7dc-0dda-4e3b-9726-b291b22f9f38",
-  //   "https://firebasestorage.googleapis.com/v0/b/quantum-68439.appspot.com/o/videos%2FTV_Studio_Camera_Sweep_In.mp4?alt=media&token=dc56a9b6-529f-41b7-a4c5-4731bd1feb2a",
-  //   "https://firebasestorage.googleapis.com/v0/b/quantum-68439.appspot.com/o/videos%2F200415_02_Batch_04_013.mp4?alt=media&token=bb567395-d0c2-4f20-9ed2-5aa1eab7d63e",
-  // ];
+  const videosSource = [
+    "https://firebasestorage.googleapis.com/v0/b/quantum-68439.appspot.com/o/videos%2F200415_01_Camera_Equipment_009.mp4?alt=media&token=3320b7dc-0dda-4e3b-9726-b291b22f9f38",
+    "https://firebasestorage.googleapis.com/v0/b/quantum-68439.appspot.com/o/videos%2FTV_Studio_Camera_Sweep_In.mp4?alt=media&token=dc56a9b6-529f-41b7-a4c5-4731bd1feb2a",
+    "https://firebasestorage.googleapis.com/v0/b/quantum-68439.appspot.com/o/videos%2F200415_02_Batch_04_013.mp4?alt=media&token=bb567395-d0c2-4f20-9ed2-5aa1eab7d63e",
+  ];
 
   const [showLetters, setshowLetters] = React.useState("");
-  // const [actualVideo, setactualVideo] = React.useState(videosSource[1]);
-  // const [videoCounter, setvideoCounter] = React.useState(1);
-  // const [showVideo, setshowVideo] = React.useState(true);
+  const [actualVideo, setactualVideo] = React.useState(videosSource[1]);
+  const [videoCounter, setvideoCounter] = React.useState(1);
+  const [showVideo, setshowVideo] = React.useState(true);
 
   const wait = async (ms) => {
     return new Promise((resolve) => {
@@ -70,17 +70,17 @@ const FirstPage = () => {
             </span>
           </p>
         </div>
-        {/* <div className="row d-flex justify-content-center">
+        <div className="row d-flex justify-content-center">
         <img
           src="https://firebasestorage.googleapis.com/v0/b/quantum-68439.appspot.com/o/resources%2FSaly-22.svg?alt=media&token=de0782d3-8808-4162-b46e-e56bb0f40edc"
           alt="test"
           width= "400"
           height = "400"
         />
-      </div> */}
+      </div>
       </div>
 
-      {/* {showVideo ? (
+      {showVideo ? (
         <div className="col-12 col-lg-6 col-xl-6 fpage-video ">
           <video
             src={actualVideo}
@@ -95,7 +95,7 @@ const FirstPage = () => {
             }}
           ></video>
         </div>
-      ) : null} */}
+      ) : null}
     </>
   );
 };
