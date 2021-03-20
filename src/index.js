590e28fd9524b0fb608f3dbc345baae92c7d0c74
rguarnizo <rguarnizo@unal.edu.co>
2021-03-19 23:36:45 -0500

    Using react three fiber

--- src/index.js
+++ src/index.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
+import {Canvas} from "react-three-fiber";
 import './index.css';
-import App from './App';
+
 import reportWebVitals from './reportWebVitals';
 
+
+const Box = () => {
+
+  const [hovered,setHovered] = useState(false);
+  const [active,setActive]   = useState(false);
+
+  return <mesh 
+  onPointerOver={()=> setHovered(true)}
+  onPointerOut={()=> setHovered(false)}
+  onClick= {()=> setActive(!active)}
+  scale={active? [1.5,1.5,1.5] : [1,1,1]}
+  >
+  <boxBufferGeometry attach="geometry" args={[10,10,5]}/>
+  <meshBasicMaterial attach="material" color= {hovered? "red":"hotpink"}/>
+        </mesh>;
+}
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Canvas>    
+    <Box/>
+    <center/>
+  </Canvas>
+ ,
   document.getElementById('root')
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
