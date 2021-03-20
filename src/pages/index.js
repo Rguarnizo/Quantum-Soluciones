3c655fc2c131ad42b9e6a746a9866a8d36ceb4e5
rguarnizo <rguarnizo@unal.edu.co>
2021-03-20 11:29:55 -0500

    Animate, rotate the cube each frame with useframe from three-fiber

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,35 @@
-import React,{useState} from "react";
+import React,{useState,useRef} from "react";
 import "./style.css";
-import { Canvas } from "react-three-fiber";
+import { Canvas, useFrame} from "react-three-fiber";
 import {useSpring,a} from 'react-spring/three';
 
 
 const Box = () => {
+
+
+  //* References
+  const meshRef = useRef();
+
   //* Init a variable, and fuction to change state
   const [hovered, setHovered] = useState(false);
   const [active, setActive] = useState(false);
+
+  //* Animate with react Spring the parameters scale and color.
   const props = useSpring({
     scale: active? [1,1,1]:[1.5,1.5,1.5],
     color: hovered? "hotpink" : "red",
   });
 
+
+  //* Each frame execute this code.
+  useFrame(()=>{
+    meshRef.current.rotation.y+=0.01;
+    meshRef.current.rotation.x+=0.01;
+  });
+
   return (
     <a.mesh
+      ref={meshRef} 
       onPointerOver={() => setHovered(true)}
       onPointerOut={() => setHovered(false)}
       onClick={() => setActive(!active)}
