d004240d2d1a2a508d4bbc4f4bef919c43d2c244
rguarnizo <rguarnizo@unal.edu.co>
2021-03-20 10:51:17 -0500

    On pointerOver an d on pointerOut, on click, to change color and scale of box

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,28 @@
-import React from "react"
-import "./style.css"
-import { Canvas } from "react-three-fiber"
+import React,{useState} from "react";
+import "./style.css";
+import { Canvas } from "react-three-fiber";
+
+
+const Box = () => {
+  //* Init a variable, and fuction to change state
+  const [hovered, setHovered] = useState(false);
+  const [active, setActive] = useState(false);
+
+  return (
+    <mesh
+      onPointerOver={() => setHovered(true)}
+      onPointerOut={() => setHovered(false)}
+      onClick={() => setActive(!active)}
+      scale={active? [1,1,1]:[1.5,1.5,1.5]}
+    >
+      <boxBufferGeometry args={[1, 1, 1]} attach="geometry" />
+      <meshBasicMaterial
+        color={hovered? "hotpink" : "red"}
+        attach="material"
+      />
+    </mesh>
+  )
+}
 
 export default function Home() {
   return (
@@ -10,11 +32,4 @@ export default function Home() {
   )
 }
 
-const Box = () => {
-  return (
-    <mesh>
-      <boxBufferGeometry args={[1, 1, 1]} attach="geometry" />
-      <meshBasicMaterial color={"red"} attach="materials"/>
-    </mesh>
-  )
-}
+
