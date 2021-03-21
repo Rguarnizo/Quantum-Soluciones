c5456fb7692129e083e00f6c8b92cd7fc08e1ba2
rguarnizo <rguarnizo@unal.edu.co>
2021-03-21 09:44:51 -0500

    Upload model and show in scene

--- src/index.js
+++ src/index.js
@@ -1,26 +1,21 @@
-import React from "react";
+import React,{Suspense} from "react";
 import ReactDOM from "react-dom";
 import './index.css';
 import {Canvas} from "react-three-fiber";
 import Box from "./components/Box";
 import Controlls from "./components/Controls";
+import Model from "./components/Model";
 
 const divRoot = document.querySelector("#root")
 
 
 ReactDOM.render(
 <Canvas onCreated={({gl})=> gl.shadowMap.enabled = true}>
-    <ambientLight/>
-    <Box position={[5,0,0]}/>
-    <Box position={[-5,0,0]}/>
-    <Controlls/>
-    
-    <spotLight penumbra={1} castShadow position={[0,10,15]} intensity={1}/>
-    <mesh rotation={[-Math.PI/2,0,0]} position={[0,-2,0]} receiveShadow>
-        <planeBufferGeometry attach="geometry" args={[200,200]} />
-        <meshPhysicalMaterial attach="material" color="white"/>
-    </mesh>
-    <Box/>
+    <ambientLight/> 
+    <Controlls />
+    <Suspense fallback={null}>
+        <Model path={'/camera-lens/scene.glb'} />
+    </Suspense>
 </Canvas>,
 divRoot
 )
