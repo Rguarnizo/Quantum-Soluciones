febd004f25338a77ce1b4c0b6dfa2c3edbd46cb2
rguarnizo <rguarnizo@unal.edu.co>
2021-03-20 20:45:50 -0500

    Creating a plane and creating OrbitControls

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import './index.css';
 import {Canvas} from "react-three-fiber";
 import Box from "./components/Box";
+import Controlls from "./components/Controls";
 
 const divRoot = document.querySelector("#root")
 
@@ -12,7 +13,13 @@ ReactDOM.render(
     <ambientLight/>
     <Box position={[5,0,0]}/>
     <Box position={[-5,0,0]}/>
+    <Controlls/>
     <fog attach="fog" args={["white",4.5,6]}/>
+    <spotLight penumbra={1}/>
+    <mesh rotation={[-Math.PI/2,0,0]}>
+        <planeBufferGeometry attach="geometry" args={[200,200]}/>
+        <meshPhysicalMaterial attach="material" color="black"/>
+    </mesh>
     <Box/>
 </Canvas>,
 divRoot
