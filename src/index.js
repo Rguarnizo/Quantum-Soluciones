e3ec6260d1e4ffc0f7f36d065c548e42a77d057e
rguarnizo <rguarnizo@unal.edu.co>
2021-03-29 13:04:06 -0500

    Trying to combine html and scene

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React,{Suspense} from "react";
 import ReactDOM from "react-dom";
 import './index.css';
 import {Canvas} from "react-three-fiber";
-import Controlls from "./components/Controls";
 import Model from "./components/Model";
 import {Html} from '@react-three/drei/web/Html';
+import NavBar from "./components/NavBar";
+import reactDom from "react-dom";
 
 
 const divRoot = document.querySelector("#root")
@@ -15,11 +16,8 @@ ReactDOM.render(
 <Canvas onCreated={({gl})=> gl.shadowMap.enabled = true} camera={{position:[0,0,200]}}>
     <ambientLight/> 
     <Suspense fallback={null}>
-        <Model path={'/camera-lens/scene.glb'} />
+        <Model path={'/camera-lens/scene.glb'}/>
     </Suspense>    
-    <Html> 
-        
-    </Html>
 </Canvas>
 <div style={{height: `${4*100}vh`}}>
 
@@ -29,5 +27,7 @@ ReactDOM.render(
 divRoot
 )
 
-
+window.onscroll((e) =>{
+    console.log(e);
+});
 
