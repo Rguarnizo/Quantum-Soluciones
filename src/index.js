df1a26c627b8eeb3d16828e715ba45c5ba0c9a70
rguarnizo <rguarnizo@unal.edu.co>
2021-03-31 16:11:59 -0500

    Por fin funciona el hp hot reload

--- src/index.js
+++ src/index.js
@@ -1,43 +1,17 @@
-import React, { useRef } from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import { TweenMax, Power3 } from "gsap";
-
-let root = document.getElementById("root");
-
-let App = () => {
-  let logos = useRef();
-
-  return (
-    <>
-      <div className="brands">
-        <a href="" className='brand b'>
-          <img src="Logos/Canon.png" alt=""  class='w-32' />
-        </a>
-        <a href="" className='brand b'>
-          <img src="Logos/Sony.png" alt=""  class='w-32' />
-        </a>
-        <a href="" className='brand b'>
-          <img src="Logos/Nikon.jpeg" alt=""  class='w-32' />
-        </a>
-        <a href="" className='brand b'>
-          <img src="Logos/GoPro.png" alt="" class='w-32'  />
-        </a>
-      </div>
-
-    <div className="circles">
-        <div className="w-24 h-24  content-center rounded-full bg-green-400  ">Green</div>
-        <div className="w-24 h-24  content-center rounded-full bg-blue-800   ">Blue</div>
-        <div className="w-24 h-24  content-center rounded-full bg-yellow-200 ">Blue</div>
-    </div>
-
-    </>
-  );
-};
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <>
+  <React.StrictMode>
     <App />
-  </>,
-  root
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
