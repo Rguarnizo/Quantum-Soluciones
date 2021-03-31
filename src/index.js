f8f007d476668e0a3c134325a98a8d95e6c91a5b
rguarnizo <rguarnizo@unal.edu.co>
2021-03-31 11:46:45 -0500

    Trying to enable hot reaload >:(

--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom'
+import React, { useRef } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { TweenMax, Power3 } from "gsap";
 
+let root = document.getElementById("root");
 
-let root = document.getElementById('root');
+let App = () => {
+  let logos = useRef();
 
+  return (
+    <>
+      <div className="brands">
+        <a href="" className='brand b'>
+          <img src="Logos/Canon.png" alt=""  class='w-32' />
+        </a>
+        <a href="" className='brand b'>
+          <img src="Logos/Sony.png" alt=""  class='w-32' />
+        </a>
+        <a href="" className='brand b'>
+          <img src="Logos/Nikon.jpeg" alt=""  class='w-32' />
+        </a>
+        <a href="" className='brand b'>
+          <img src="Logos/GoPro.png" alt="" class='w-32'  />
+        </a>
+      </div>
+
+    <div className="circles">
+        <div className="w-24 h-24  content-center rounded-full bg-green-400  ">Green</div>
+        <div className="w-24 h-24  content-center rounded-full bg-blue-800   ">Blue</div>
+        <div className="w-24 h-24  content-center rounded-full bg-yellow-200 ">Blue</div>
+    </div>
+
+    </>
+  );
+};
 
 ReactDOM.render(
-    <>
-    <button></button>
-    <div className="brands"><img src="Logos/Canon.png" width={800} height={500} alt=""/></div>
-    <div className="brands"><img src="Logos/Nikon.jpeg" width={800} height={800} alt=""/></div>
-    <div className="brands"><img src="Logos/Sony.png" width={800} height={800} alt=""/></div>
-    <div className="brands"><img src="Logos/GoPro.png" width={800} height={800} alt=""/></div>
-    </>,
-    root
-)
+  <>
+    <App />
+  </>,
+  root
+);
