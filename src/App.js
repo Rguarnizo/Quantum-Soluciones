14fe8798a6b95e74150688eb990a9484d640cb83
rguarnizo <rguarnizo@unal.edu.co>
2021-04-05 18:33:39 -0500

    Using gsap to pin sections with scroll

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import "./App.css";
+import React, { useEffect } from "react";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
 
 function App() {
+  gsap.registerPlugin(ScrollTrigger);
+
+  useEffect(() => {
+    let sections = gsap.utils.toArray("section");
+
+    sections.forEach((section) => {
+      gsap.to(section, { scrollTrigger: { pin: section, end: "100%" } });
+    });
+  });
+
   return (
     <>
-      <section className="Home        h-screen bg-green-500"></section>
-      <section className="Promociones h-screen bg-blue-500"></section>
-      <section className="Productos   h-screen bg-red-500"></section>
-      <section className="Ubicacion   h-screen bg-yellow-500"></section>
+      <section className="Home        h-screen bg-green-500   "></section>
+      <section className="Promociones h-screen bg-blue-500    "></section>
+      <section className="Productos   h-screen bg-red-500     "></section>
+      <section className="Ubicacion   h-screen bg-yellow-500  "></section>
       <section className="Footer h-1/6 bg-white"></section>
     </>
   );
