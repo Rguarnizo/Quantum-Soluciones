611df1ea1c775b222a30b52faa552e3bfb0b254c
rguarnizo <rguarnizo@unal.edu.co>
2021-04-05 09:21:18 -0500

    Instalacion de gsap y react-three-fiber para animaciones

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <section className="Promociones h-screen bg-blue-500"></section>
       <section className="Productos   h-screen bg-red-500"></section>
       <section className="Ubicacion   h-screen bg-yellow-500"></section>
-      <section className="Footer"></section>
+      <section className="Footer h-1/6 bg-white"></section>
     </>
   );
 }
