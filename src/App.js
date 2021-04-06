adacefa36a811102cb86952f255281d899ecd3a7
rguarnizo <rguarnizo@unal.edu.co>
2021-04-05 19:27:40 -0500

    More evident pin sections

--- src/App.js
+++ src/App.js
@@ -6,21 +6,34 @@ import ScrollTrigger from "gsap/ScrollTrigger";
 function App() {
   gsap.registerPlugin(ScrollTrigger);
 
-  useEffect(() => {
-    let sections = gsap.utils.toArray("section");
+  let tl; 
 
+  useEffect(() => {
+    
+    let sections = gsap.utils.toArray("section");
+    tl = gsap.timeline();
     sections.forEach((section) => {
-      gsap.to(section, { scrollTrigger: { pin: section, end: "100%" } });
+      tl.to(section, { scrollTrigger: { pin: section, end: "100%" } });
     });
   });
 
   return (
     <>
-      <section className="Home        h-screen bg-green-500   "></section>
-      <section className="Promociones h-screen bg-blue-500    "></section>
-      <section className="Productos   h-screen bg-red-500     "></section>
-      <section className="Ubicacion   h-screen bg-yellow-500  "></section>
-      <section className="Footer h-1/6 bg-white"></section>
+      <section className="Home        h-screen bg-green-500   ">
+        <div className="box bg-white h-32 w-32"></div>
+      </section>
+      <section className="Promociones h-screen bg-blue-500    ">
+        <div className="box bg-white h-32 w-32"></div>
+      </section>
+      <section className="Productos   h-screen bg-red-500     ">
+        <div className="box bg-white h-32 w-32"></div>
+      </section>
+      <section className="Ubicacion   h-screen bg-yellow-500  ">
+        <div className="box bg-white h-32 w-32"></div>
+      </section>
+      <section className="Footer h-1/6 bg-white">
+      <div className="box bg-white h-32 w-32"></div>
+      </section>
     </>
   );
 }
