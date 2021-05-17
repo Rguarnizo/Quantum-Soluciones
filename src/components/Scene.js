2ed801f923baad942c261271bec5635c02948b5d
rguarnizo <rguarnizo@unal.edu.co>
2021-05-16 19:53:28 -0500

    Trying to upload drone with animations model

--- src/components/Scene.js
+++ src/components/Scene.js
@@ -2,7 +2,8 @@ import React, { Suspense, useEffect, useRef, useState } from "react";
 import { Canvas, useThree, useFrame } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import EscenaProductos from "./EscenaProductos";
-
+import Drone from "./Drone";
+import DroneR from "./DroneR";
 
 
 
@@ -10,7 +11,12 @@ export default function Scene() {
   return (
     <Canvas className="EscenaProductos w-2/6">
         
-        <EscenaProductos />
+            <EscenaProductos />
+      
+    <Suspense fallback={null}>
+        <DroneR/>
+    </Suspense>
+      
     {/* <Suspense fallback={null}>
           <Tokyo/>
         </Suspense> */}
