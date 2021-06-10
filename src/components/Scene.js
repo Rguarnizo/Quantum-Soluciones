6d3f90fd457aa302839c64b9e374ac4a4a6b13e0
rguarnizo <rguarnizo@unal.edu.co>
2021-06-09 20:52:31 -0500

    Workingggggg! FUNCIONANDOOO! Al fin :)

--- src/components/Scene.js
+++ src/components/Scene.js
@@ -1,75 +1,83 @@
 import React, { Suspense, useEffect, useRef, useState } from "react";
 import { Canvas, useThree, useFrame } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
-import * as THREE from 'three';
+import * as THREE from "three";
 import EscenaProductos from "./EscenaProductos";
 import Drone from "./Drone";
 import DroneR from "./DroneR";
 import DJI from "./DJI";
 import DjiDrone from "./DJIDrone";
-import { useLoader } from '@react-three/fiber'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { useLoader } from "@react-three/fiber";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import gsap from "gsap";
 
-function Model(){
-
-  const gltf = useLoader(GLTFLoader,"Models/Dji/DjiDrone.gltf");
+function Model() {
+  const gltf = useLoader(GLTFLoader, "Models/Dji/DjiDrone.gltf");
   const mixer = new THREE.AnimationMixer();
   const clock = new THREE.Clock();
   const animation = useRef();
-  
-  useEffect(()=>{
-    mixer.clipAction(gltf.animations[0],gltf.scene);
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".Productos",
+      scrub: 1,
+      start: "top top",
+      end: "100%",
+    },
+  });
+
+  useEffect(() => {
+    animation.current = mixer.clipAction(gltf.animations[0], gltf.scene);
+    // animation.current.setLoop(THREE.LoopOnce);
+    animation.current.play();
     
-  });
+    tl.to(
+      animation.current,
+      { time: 23, onUpdate: function() {mixer.update(0.01)}},
+      0
+    );
+  }, []);
 
-  useEffect(()=>{
-    console.log(animation.current);
-    let tl = gsap.timeline({scrollTrigger: {
-      trigger:".Productos",
-      scrub: 1, 
-      start: 'top top',
-      end: '100%'
-    }});
+  useEffect(() => {
     setTimeout(() => {
-      tl.to(gltf.scene.rotation,{
-        
-        y:Math.PI/2,
-          
-      },0).to(mixer,{time:23},0);
+      tl.to(
+        gltf.scene.rotation,
+        {
+          y: Math.PI / 2,
+        },
+        0
+      );
     });
-  },[]);
+  }, []);
 
-  useFrame(()=>{
-  
-    const delta = clock.getDelta();
-    mixer.update(delta);
+  useFrame(()=> {
+    mixer.time += 0.01;
   });
 
-  return<>
-    <primitive object={gltf.scene}/>
-  </>
-}
-
-
-export default function Scene() {
 
   return (
+    <>
+      <primitive object={gltf.scene} />
+    </>
+  );
+}
+
+export default function Scene() {
+  return (
     <Canvas className="EscenaProductos w-2/6" color={0x000000}>
-        <OrbitControls/>
-        <spotLight position={[5,5,0]}  intensity={10} castShadow/>
-        <spotLight position={[-5,5,0]} intensity={10} castShadow/>
-        <spotLight position={[-5,-5,0]} intensity={10} castShadow/>
-        <spotLight position={[5,-5,0]} intensity={10} castShadow/>
-        <hemisphereLight args={[0xffeeb1,0x080820,4]} position={[5,5,0]}/>
-            {/* <EscenaProductos /> */}
-      
-    <Suspense fallback={null}>
+      <OrbitControls />
+      <spotLight position={[5, 5, 0]} intensity={10} castShadow />
+      <spotLight position={[-5, 5, 0]} intensity={10} castShadow />
+      <spotLight position={[-5, -5, 0]} intensity={10} castShadow />
+      <spotLight position={[5, -5, 0]} intensity={10} castShadow />
+      <hemisphereLight args={[0xffeeb1, 0x080820, 4]} position={[5, 5, 0]} />
+      {/* <EscenaProductos /> */}
+
+      <Suspense fallback={null}>
         {/* <Drone/> */}
-        <Model/>
-    </Suspense>
-      
-    {/* <Suspense fallback={null}>
+        <Model />
+      </Suspense>
+
+      {/* <Suspense fallback={null}>
           <Tokyo/>
         </Suspense> */}
     </Canvas>
