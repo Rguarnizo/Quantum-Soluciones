516bfc3c50e89abaa208319cfb216ca2dc26d075
rguarnizo <rguarnizo@unal.edu.co>
2021-07-28 21:44:54 -0500

    Changing distribution of Maintenance services and add animations

---
+++ src/Components/Scene.jsx
@@ -0,0 +1,109 @@
+import React, { Suspense, useEffect, useRef, useState } from "react";
+import { Canvas, useThree, useFrame } from "@react-three/fiber";
+import * as THREE from "three";
+import { useLoader } from "@react-three/fiber";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import {MeshoptDecoder} from "three/examples/jsm/libs/meshopt_decoder.module.js"
+import {DRACOLoader} from "three/examples/jsm/loaders/DRACOLoader"
+
+
+
+import gsap from "gsap";
+
+function Model() {
+
+  const dracoLoader = new DRACOLoader();
+
+  // const loader = new GLTFLoader();
+  // loader.setMeshoptDecoder(MeshoptDecoder);
+  // loader.setKTX2Loader(KTX2Loader);  
+  // loader.setDRACOLoader(dracoLoader);
+  
+  let gltf = useLoader(GLTFLoader, "Models/Dji/DJIDrone.gltf",(loader)=>{
+    console.log(loader);
+    loader.setMeshoptDecoder(MeshoptDecoder);
+    loader.setDRACOLoader(dracoLoader);
+  });
+  
+  const mixer = new THREE.AnimationMixer();
+  const clock = new THREE.Clock();
+  const animation = useRef();
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: "#maintenance",
+      scrub: 3,
+      start: "top top",
+      pin: "#maintenance",
+      end: "+=400%",
+      markers:true
+    },
+  });
+
+  useEffect(() => {
+    animation.current = mixer.clipAction(gltf.animations[0], gltf.scene);
+    // animation.current.setLoop(THREE.LoopOnce);
+    animation.current.play();
+    
+    tl.to(
+      animation.current,
+      { time: 21, onUpdate: function() {mixer.update(0.1)}},
+      0
+    );
+  }, []);
+
+  useEffect(() => {
+    setTimeout(() => {
+      tl.to(
+        gltf.scene.rotation,
+        {
+          y: Math.PI / 4,
+          scrollTrigger: {
+            end: "+=100%"
+          }
+        },
+        0
+      );
+      
+        tl.fromTo(".brands",{
+          xPercent: 100           
+        },{
+          xPercent: 0
+        },0)
+    });
+  }, []);
+
+  useFrame(()=> {
+    mixer.time += 0.01;
+  });
+
+
+  return (
+    <>
+      <primitive object={gltf.scene} />
+    </>
+  );
+}
+
+export default function Scene() {
+  return (<>
+  
+    
+    <Canvas id="canvas" className="EscenaProductos w-full h-full" color={0x000000} camera={{position:[0,0,3]}}>
+      <spotLight position={[5, 5, 0]} intensity={10} castShadow />
+      <spotLight position={[-5, 5, 0]} intensity={10} castShadow />
+      <spotLight position={[-5, -5, 0]} intensity={10} castShadow />
+      <spotLight position={[5, -5, 0]} intensity={10} castShadow />
+      <hemisphereLight args={[0xffeeb1, 0x080820, 4]} position={[5, 5, 0]} />
+      {/* <EscenaProductos /> */}
+
+      <Suspense fallback={null}>
+        {/* <Drone/> */}
+        <Model />
+      </Suspense>
+      {/* <Suspense fallback={null}>
+          <Tokyo/>
+        </Suspense> */}
+    </Canvas>
+    </>
+  );
+}
--- src/Pages/MaintenanceServices.jsx
+++ src/Pages/MaintenanceServices.jsx
@@ -1,20 +1,42 @@
 import React from "react";
 import styled from "styled-components";
 import Scene from "../Components/Scene";
+import gsap from "gsap";
+
 const MaintenanceServices = () => {
   const Text = styled.span`
     font-family: "DM Sans", sans-serif;
   `;
 
-  const QuantumSubtitleClasses = `text-xl xl:text-2xl font-semibold uppercase dark:text-blue-100 tracking-wider antialiased mb-6 xl:mb-12 mt-16 xl:mt-0`;
-  const QuantumTitleClasses = `font-bold text-5xl xl:text-6xl tracking-tighter mb-6 dark:text-blue-100 max-w-full`;
-  const QuantumDescriptionClasses = `font-normal  tracking-tighter dark:text-blue-100 `;
+  const QuantumSubtitleClasses = `text-xl xl:text-2xl font-semibold uppercase dark:text-blue-100 tracking-wider antialiased mb-6 xl:mb-12 mt-16 xl:mt-0 subtitle`;
+  const QuantumTitleClasses = `font-bold text-5xl xl:text-6xl tracking-tighter mb-6 dark:text-blue-100 max-w-full title`;
+  const QuantumDescriptionClasses = `font-normal  tracking-tighter dark:text-blue-100 description`;
+
+  React.useEffect(()=>{
+    let tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#maintenance",
+        scrub: 1,
+        start: "top 30%",
+        end: "+=400%",
+        markers:true,
+      },
+    }
+    );
+
+    tl.fromTo(".part-1",{alpha:0},{alpha:1})
+    .fromTo(".part-2",{alpha:0},{alpha:1})
+    .to(".part-1",{alpha:0},"<")
+    .fromTo(".part-3",{alpha:0},{alpha:1},"+=0.5")
+    .to(".part-2",{alpha:0},"<");
+
+  })
 
   return (
-    <section id="maintenance" className=" px-5 py-20 md:px-16 dark:bg-gray-800 ">
-      <div className="grid xl:grid-rows-2 pt-20 ">
-        <div className="grid grid-cols-2 w-screen h-screen ">
-          <div className="flex flex-col col-span-2 xl:col-span-1">
+    <div id="maintenance" className=" py-4 px-5 md:px-16 dark:bg-gray-800 ">
+      <div className="grid pt-4 ">
+        <div className="flex flex-col lg:flex-row w-screen h-screen">
+          <div className="flex flex-col col-span-2 mt-10 xl:mt-0 xl:pl-16 xl:col-span-1 part-1">
             <Text className={QuantumSubtitleClasses}>SOLUCIONES QUANTUM</Text>
             <Text className={QuantumTitleClasses}>
               {" "}
@@ -27,12 +49,13 @@ const MaintenanceServices = () => {
               equipos y accesorios profesionales
             </Text>
           </div>
-          <div className="img-wrapper h-full w-full">
+          <div className="img-wrapper w-full h-full">
             <Scene/>
           </div>
-        </div>
-        <div className="grid xl:grid-cols-2 mt-10 xl:pl-16">
-          <div className="flex flex-col gap-10">
+          
+
+          <div className="grid xl:grid-cols-2 absolute">
+          <div className="flex flex-col gap-10 mt-10 h-screen xl:mt-0 xl:pl-16 part-2">
             <Text
               className={
                 QuantumDescriptionClasses + "font-bold max-w-xl xl:text-xl"
@@ -60,7 +83,7 @@ const MaintenanceServices = () => {
               </Text>
             </Text>
           </div>
-          <div className="flex flex-col gap-10 mt-10 xl:mt-0">
+          <div className="flex flex-col gap-10 mt-10 xl:mt-0 xl:pl-16 h-screen absolute part-3">
             <Text
               className={
                 QuantumDescriptionClasses + "font-bold max-w-xl xl:text-xl"
@@ -89,8 +112,12 @@ const MaintenanceServices = () => {
             </Text>
           </div>
         </div>
+
+
+          
+        </div>
       </div>
-    </section>
+    </div>
   );
 };
 
