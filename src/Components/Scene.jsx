a0e2eeeb6756d0c2d6d4a0345ef81fa5f610aba7
rguarnizo <rguarnizo@unal.edu.co>
2021-08-04 20:15:12 -0500

    Extract GLB file model from firebase Storage

--- src/Components/Scene.jsx
+++ src/Components/Scene.jsx
@@ -7,35 +7,27 @@ import {useStorage} from "reactfire";
 export default function Scene() {
   const storage = useStorage();
   const [url,setUrl] = useState();
-
+  const [loading,setLoading] = useState(true);
 
   useEffect(()=>{
     storage.ref("/DJIDrone.glb").getDownloadURL().then((downloadURL) => {
-      
-      setUrl(downloadURL);
-
-      var xhr = new XMLHttpRequest();
-      xhr.responseType = 'blob';
-      xhr.onload = function(event) {
-        var blob = xhr.response;
-        console.log("blob:",blob);
-      };
-      xhr.open('GET',downloadURL);
-      xhr.send();
+      console.log(downloadURL);
+      setUrl(downloadURL);      
+      setLoading(false);
     });
   })
   
   
-  return (<>  
+  return loading? <div></div>:(<>  
     <Canvas id="canvas" className="EscenaProductos " color={0x000000} camera={{position:[0,0,3]}}>
       <spotLight position={[5, 5, 0]} intensity={5} castShadow />
       <spotLight position={[-5, 5, 0]} intensity={5} castShadow />
       <spotLight position={[-5, -5, 0]} intensity={5} castShadow />
       <spotLight position={[5, -5, 0]} intensity={5} castShadow />
       <hemisphereLight args={[0xffeeb1, 0x080820, 4]} position={[5, 5, 0]} />
-      {/* <Suspense fallback={null}>
+      <Suspense fallback={null}>
         <DronDJI url={url}/>
-      </Suspense> */}
+      </Suspense>
     </Canvas>
     </>
   );
